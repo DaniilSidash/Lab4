fbac1d1712d32522535c12fb9bdea4f42601f694
DaniilSidash <70960703+DaniilSidash@users.noreply.github.com>
2020-12-13 22:25:34 +0300

    Add files via upload

---
+++ Лаб4 индивидуал№2.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    Word = input("Введите словосочетание ")
+    for i in range(len(Word)):
+         if (Word[i] == 'ч' or Word[i] == 'щ') and Word[i + 1] != 'а':
+            input("Вы написали все верно")
+    else:
+        r =Word.replace('у', 'а')
+        print("Исправленное букво сочетание:", r)
+        input("Вы написали буквосочетание неверно ")
+
+
+
+
+
+
+
