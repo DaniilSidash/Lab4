71f42641b1bab23a957ad228334c17e598cf0f3d
DaniilSidash <70960703+DaniilSidash@users.noreply.github.com>
2020-12-13 22:22:02 +0300

    Add files via upload

---
+++ Лаб4 Индивидуальное№1.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+     s = list(input('Напечатайте предложение: ').split())
+     c = input('Введите символы: ')
+for i in range(len(s)):
+            if(c in s[i]): print (s[i])
+
+
+
+
+
+
+
---
+++ Лаб4 индивидуал 3.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    word= "ИТЕРНЕТН"
+    print(word[0]+word[7:]+word[-2]+word[-3]+word[-5]+word[-1]+word[-3]+word[-2] )
+
