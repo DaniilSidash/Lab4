b878551e9bf9bfa8076a374ec8b57e915125388f
DaniilSidash <70960703+DaniilSidash@users.noreply.github.com>
2020-12-22 21:06:41 +0300

    Update Лаб4 индивидуал№2.py

--- Лаб4 индивидуал№2.py
+++ Лаб4 индивидуал№2.py
@@ -9,12 +9,13 @@
 if __name__ == '__main__':
     Word = input("Введите словосочетание ")
     for i in range(len(Word)):
-         if (Word[i] == 'ч' or Word[i] == 'щ') and Word[i + 1] != 'а':
-            input("Вы написали все верно")
-    else:
-        r =Word.replace('е', 'а') and Word.replace('у', 'а')
-        print("Исправленное букво сочетание:", r)
-        input("Вы написали буквосочетание неверно ")
+        if (Word[-1] == 'ч' or Word[-1] == 'щ') and Word[2] != 'а':
+           input("Вы написали все верно")
+        else:
+           r = Word.replace('щя','ща')
+           b = Word.replace('чя', 'ча')
+        print("Исправленное буквосочетание:",r,b)
+        input("Вы написали буквосочетание Ча Ща неверно ")
 
 
 
